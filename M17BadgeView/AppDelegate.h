9834b99a0a42d05df50662ef3243b7005f368144
chengbin <chengbinz@jumei.com>
2017-10-11 15:14:36 +0800

    Initial Commit

---
+++ M17BadgeView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  M17BadgeView
+//
+//  Created by chengbin on 11/10/2017.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
